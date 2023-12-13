3a637179e959f73484b9f4dcca9e116f203e8a0e
Tolga Kayis <tolgakayis_@hotmail.com>
2023-12-13 14:13:17 +0300

    Add project files.

---
+++ Entities/Concretes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concretes
+{
+    public class User : Entity<Guid>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+    }
+}
