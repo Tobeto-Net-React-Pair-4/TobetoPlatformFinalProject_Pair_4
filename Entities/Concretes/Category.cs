021f902af923292469be567ea43773079f15ba2c
Tolga Kayis <tolgakayis_@hotmail.com>
2023-12-13 14:38:54 +0300

    Category entitilendi

--- Entities/Concretes/Category.cs
+++ Entities/Concretes/Category.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities;
 
 namespace Entities.Concretes
 {
-    public class Category
+    public class Category : Entity<Guid>
     {
     }
 }
