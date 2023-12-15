03cd0989098bd53acd10b7523b86165a3a99d423
Tolga Kayış <tolgakayis_@hotmail.com>
2023-12-15 16:28:58 +0300

    Business Layer progress

---
+++ Business/Abstracts/ICourseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess.Paging;
+
+namespace Business.Abstracts
+{
+    public interface ICourseService
+    {
+        Task<Paginate<GetListedCourseResponse>> GetListAsync();
+        Task<CreatedCourseResponse> Add(CreateCourseRequest createCourseRequest);
+        Task<UpdatedCourseResponse> Update(UpdateCourseRequest updateCourseRequest);
+        Task<DeletedCourseResponse> Delete(DeleteCourseRequest deleteCourseRequest);
+    }
+}
---
+++ Business/Rules/CategoryBusinessRules.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules
+{
+    public class CategoryBusinessRules : BaseBusinessRules
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryBusinessRules(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public async Task MaximumCountIsTen()
+        {
+            var result = await _categoryDal.GetListAsync();
+
+            if (result.Count >= 10)
+            {
+                throw new Exception("Category limit is reached");
+            }
+        }
+    }
+}
