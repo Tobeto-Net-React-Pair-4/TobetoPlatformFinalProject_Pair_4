e5f3140bba3eee8c4ce5ef374ad792daad70cf04
cemalsezer <infocemalsezer@gmail.com>
2023-12-16 10:26:31 +0300

    new prop added to dataaccess

---
+++ DataAccess/Concretes/EfInstructorDal.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes
+{
+    public class EfInstructorDal 
+    {
+        public EfInstructorDal()
+        {
+            
+        }
+        public EfInstructorDal(int s1)
+        {
+            
+        }
+    }
+}
