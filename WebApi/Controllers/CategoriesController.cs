1a2a3ea10b84aa05be079cefbb754304ba594bd5
Nisa Yavuz <nsnryvz@outlook.com>
2023-12-16 16:25:40 +0300

    WebApi layer is updated.
    Controller layer files are updated.

---
+++ WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Category.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] CreateCategoryRequest createCategoryRequest)
+        {
+            var result = await _categoryService.Add(createCategoryRequest);
+            return Ok(result);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody] UpdateCategoryRequest updateCategoryRequest)
+        {
+            var result = await _categoryService.Update(updateCategoryRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete([FromBody] DeleteCategoryRequest deleteCategoryRequest)
+        {
+            var result = await _categoryService.Delete(deleteCategoryRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _categoryService.GetListAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Course.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        ICourseService _courseService;
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] CreateCourseRequest createCourseRequest)
+        {
+            var result = await _courseService.Add(createCourseRequest);
+            return Ok(result);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody] UpdateCourseRequest updateCourseRequest)
+        {
+            var result = await _courseService.Update(updateCourseRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete([FromBody] DeleteCourseRequest deleteCourseRequest)
+        {
+            var result = await _courseService.Delete(deleteCourseRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _courseService.GetListAsync();
+            return Ok(result);
+        }
+    }
+}
