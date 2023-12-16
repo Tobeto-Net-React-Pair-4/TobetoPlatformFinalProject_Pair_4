4992fee429fce248cbf69127523dcfc2d247133d
Nisa Yavuz <nsnryvz@outlook.com>
2023-12-16 19:32:38 +0300

    Core layer is updated.
    Core layer is updated.

---
+++ Core/CrossCuttingConcerns/Exceptions/Extension/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extension
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+            => app.UseMiddleware<ExceptionMiddleware>();
+    }
+}
---
+++ Core/CrossCuttingConcerns/Exceptions/Extension/ProblemDetailsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extension
+{
+    public static class ProblemDetailsExtensions
+    {
+        public static string AsJson<TProblemDetail>(this TProblemDetail details)
+            where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(details);
+    }
+}
