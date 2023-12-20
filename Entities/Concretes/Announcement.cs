29b61cda72e2f5136dc7bb970f0e2874c8dd37e5
Nisa Yavuz <nsnryvz@outlook.com>
2023-12-20 20:45:40 +0300

    Entities are added.
     Announcement and Appeal entities are added.

---
+++ Entities/Concretes/Announcement.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Announcement : Entity<Guid>
+    {
+        public string Title { get; set; }
+        public string OrganizationName { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
---
+++ Entities/Concretes/Appeal.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Appeal : Entity<Guid>
+    {
+        public string Title { get; set; }
+        public string Form { get; set; }
+        public string File { get; set; }
+        public string AppealStatus { get; set; }
+        public string FormStatus { get; set; }
+        public string FileStatus { get; set; }
+        public List<User> Users { get; set; }
+
+
+    }
+}
