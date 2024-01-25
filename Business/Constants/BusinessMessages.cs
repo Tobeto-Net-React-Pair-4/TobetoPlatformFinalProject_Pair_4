b3aea61e4f4984572679de7fec345cb5028d0e06
cemalsezer <infocemalsezer@gmail.com>
2024-01-25 18:47:26 +0300

    Auth Business Rules are updated

--- Business/Constants/BusinessMessages.cs
+++ Business/Constants/BusinessMessages.cs
@@ -12,7 +12,9 @@ namespace Business.Constants
         public static string CategoryCourseLimit = "Bir kategoride 20 adetten fazla kurs olamaz";
         public static string CourseNameAlreadyExists = "Bu kurs adında zaten bir kurs bulunmaktadır";
         public static string AuthorizationDenied = "Yetkiniz yok";
-        public static string LoginError = "Giriş hatalı";
+        public static string LoginPasswordError = "Şifrenizi kontrol ediniz";
+        public static string LoginSuccess = "Giriş başarılı";
+        public static string PasswordNull = "Parola boş olmamalı";
         public static string UserExists = "Kullanıcı kayıtlı";
         public static string UserNotExists = "Kullanıcı kayıtlı değil";
 
