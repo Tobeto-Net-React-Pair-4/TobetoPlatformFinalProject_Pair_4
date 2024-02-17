6400e37fd414e69d4a8fa707f820b1f71bffec41
CemalSezer <infocemalsezer@gmail.com>
2024-02-17 18:15:17 +0300

    New abstracts are updated in Business Layer
    CourseLiveContent, Homework, LiveContent, Session, UserCalendar

---
+++ Business/Abstracts/IHomeworkService.cs
@@ -0,0 +1,20 @@
+﻿using Business.Dtos.Homework.Requests;
+using Business.Dtos.Homework.Responses;
+using Core.DataAccess.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IHomeworkService
+    {
+        Task<Paginate<GetListHomeworkResponse>> GetListAsync();
+        Task<CreatedHomeworkResponse> AddAsync(CreateHomeworkRequest createHomeworkRequest);
+        Task<UpdatedHomeworkResponse> UpdateAsync(UpdateHomeworkRequest updateHomeworkRequest);
+        Task<DeletedHomeworkResponse> DeleteAsync(Guid homeworkId);
+        Task<GetHomeworkResponse> GetByIdAsync(Guid homeworkId);
+    }
+}
---
+++ Business/Abstracts/ILiveContentService.cs
@@ -0,0 +1,26 @@
+﻿using Business.Dtos.CourseLiveContent.Requests;
+using Business.Dtos.CourseLiveContent.Responses;
+using Business.Dtos.LiveContent.Requests;
+using Business.Dtos.LiveContent.Responses;
+using Business.Dtos.UserCourse.Requests;
+using Business.Dtos.UserCourse.Responses;
+using Core.DataAccess.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface ILiveContentService
+    {
+        Task<Paginate<GetListLiveContentResponse>> GetListAsync();
+        Task<CreatedLiveContentResponse> AddAsync(CreateLiveContentRequest createLiveContentRequest);
+        Task<UpdatedLiveContentResponse> UpdateAsync(UpdateLiveContentRequest updateLiveContentRequest);
+        Task<DeletedLiveContentResponse> DeleteAsync(Guid liveContentId);
+        Task<GetLiveContentResponse> GetByIdAsync(Guid liveContentId);
+        Task<CreatedCourseLiveContentResponse> AssignContentAsync(CreateCourseLiveContentRequest createCourseLiveContentRequest);
+
+    }
+}
