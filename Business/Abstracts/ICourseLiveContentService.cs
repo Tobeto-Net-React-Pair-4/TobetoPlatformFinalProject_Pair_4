9c867e3e7a0a00ca08bc93d73b5ba62f6bf09d5e
CemalSezer <infocemalsezer@gmail.com>
2024-02-17 23:07:27 +0300

    CourseLiveContentService is updated

--- Business/Abstracts/ICourseLiveContentService.cs
+++ Business/Abstracts/ICourseLiveContentService.cs
@@ -8,7 +8,7 @@ namespace Business.Abstracts
     public interface ICourseLiveContentService
     {
         Task<CreatedCourseLiveContentResponse> AddAsync(CreateCourseLiveContentRequest createCourseLiveContentRequest);
-        Task<UpdatedCourseLiveContent> UpdateAsync(UpdateCourseLiveContentRequest updateCourseLiveContentRequest);
+        Task<UpdatedCourseLiveContentResponse> UpdateAsync(UpdateCourseLiveContentRequest updateCourseLiveContentRequest);
         Task<DeletedCourseLiveContentResponse> DeleteAsync(DeleteCourseLiveContentRequest deleteCourseLiveContentRequest);
         Task<GetCourseLiveContentResponse> GetAsync(GetCourseLiveContentRequest getCourseLiveContentRequest);
         Task<Paginate<GetListCourseLiveContentResponse>> GetListAsync();
