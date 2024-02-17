de05169c5835803a0a94e4a57a6ec5c172f99834
M.ilyast <muhammedilyastuman@gmail.com>
2024-02-17 17:07:28 +0300

    abstract added

---
+++ Business/Abstracts/IAssignment.cs
@@ -0,0 +1,15 @@
+﻿using Business.Dtos.Assignment.Requests;
+using Business.Dtos.Assignment.Responses;
+using Core.DataAccess.Paging;
+
+namespace Business.Abstracts
+{
+    public interface IAssignment
+    {
+        Task<Paginate<GetListAssignmentResponse>> GetListAsync();
+        Task<CreatedAssignmentResponse> AddAsync(CreateAssignmentRequest createAssignmentRequest);
+        Task<UpdatedAssignmentResponse> UpdateAsync(UpdateAssignmentRequest updateAssignmentRequest);
+        Task<DeletedAssignmentResponse> DeleteAsync(Guid Id);
+        Task<GetAssignmentResponse> GetByIdAsync(Guid Id);
+    }
+}
--- Business/Abstracts/IAsyncContent.cs
+++ Business/Abstracts/IAsyncContent.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.Profiles
+namespace Business.Abstracts
 {
-    internal class Class1
+    internal interface IAsyncContent
     {
     }
 }
