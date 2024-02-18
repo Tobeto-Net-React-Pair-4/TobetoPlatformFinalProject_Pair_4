cdcabb01ac7f742ab47638e67deb936cfa0bab59
CemalSezer <infocemalsezer@gmail.com>
2024-02-18 18:38:10 +0300

    useroperationclaim dto is updated

---
+++ Business/Dtos/UserOperationClaim/Responses/DeletedUserOperationClaimResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.UserOperationClaim.Responses
+{
+    public class DeletedUserOperationClaimResponse
+    {
+        public Guid UuserId { get; set; }
+        public Guid OperationClaimId { get; set; }
+    }
+}
