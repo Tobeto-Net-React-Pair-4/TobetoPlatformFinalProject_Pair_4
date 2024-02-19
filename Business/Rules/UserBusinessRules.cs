ba97daad645b33c374b616e1baf0cd9d41441deb
CemalSezer <infocemalsezer@gmail.com>
2024-02-19 15:18:19 +0300

    UserBusinessrules is updated

--- Business/Rules/UserBusinessRules.cs
+++ Business/Rules/UserBusinessRules.cs
@@ -8,30 +8,30 @@ using Entities.Concretes;
 
 namespace Business.Rules
 {
-    public class UserBusinessRules : BaseBusinessRules<User>
-    {
-        IUserDal _userDal;
+    //public class UserBusinessRules : BaseBusinessRules<User>
+    //{
+    //    IUserDal _userDal;
 
-        public UserBusinessRules(IUserDal userDal) : base(userDal)
-        {
-           _userDal = userDal;
-        }
+    //    public UserBusinessRules(IUserDal userDal) : base(userDal)
+    //    {
+    //       _userDal = userDal;
+    //    }
 
-        public async Task OldPassword(UpdateUserRequest updateUserRequest)
-        {
-            var result = await _userDal.GetListAsync();
-            foreach (var user in result.Items)
-            {
-                if (user.Id == updateUserRequest.Id)
-                {
-                    if (user.PasswordHash == updateUserRequest.Password)
-                    {
-                        throw new BusinessException(BusinessMessages.OldPassword);
+    //    public async Task OldPassword(UpdateUserRequest updateUserRequest)
+    //    {
+    //        var result = await _userDal.GetListAsync();
+    //        foreach (var user in result.Items)
+    //        {
+    //            if (user.Id == updateUserRequest.Id)
+    //            {
+    //                if (user.PasswordHash == updateUserRequest.Password)
+    //                {
+    //                    throw new BusinessException(BusinessMessages.OldPassword);
 
-                    }
-                }
-            }
+    //                }
+    //            }
+    //        }
 
-        }
-    }
+    //    }
+    //}
 }
