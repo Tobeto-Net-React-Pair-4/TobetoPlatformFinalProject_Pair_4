0e1a4de8f861945cab34d4b77792a8d70015f536
CemalSezer <infocemalsezer@gmail.com>
2024-02-20 14:43:37 +0300

    Business rules are updated
    ForeignLangage, Instructor, OperationClaim, PersonalInfo

---
+++ Business/Rules/OperationClaimBusinessRules.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstracts;
+using Core.Business.Rules;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules
+{
+    public class OperationClaimBusinessRules : BaseBusinessRules<OperationClaim>
+    {
+        private readonly IOperationClaimDal _operationClaimDal;
+        private readonly IOperationClaimService _operationClaimService;
+        public OperationClaimBusinessRules(IOperationClaimDal operationClaimDal, IOperationClaimService operationClaimService) : base(operationClaimDal)
+        {
+            _operationClaimDal = operationClaimDal;
+            _operationClaimService = operationClaimService;
+        }
+    }
+}
